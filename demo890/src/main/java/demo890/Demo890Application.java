90813089c3c06909aa98fdd940942ad114be891f
babu <babu@gmail.com>
2022-10-29 23:17:22 +0530

    Hi springBoot Recent

---
+++ demo890/src/main/java/demo890/Demo890Application.java
@@ -0,0 +1,13 @@
+package demo890;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo890Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo890Application.class, args);
+	}
+
+}
---
+++ demo890/src/test/java/demo890/Demo890ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo890;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo890ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
